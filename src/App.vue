9dabab604e891a7f103c953d61aa748070936684
Wasin <xelticross@gmail.com>
2022-11-02 11:16:11 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,77 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="page-container">
+    <nav
+      id="mainNavbar"
+      class="navbar navbar-dark navbar-expand-md position-fixed"
+    >
+      <a href="#" class="navbar-brand" style="color: red"
+        ><img src="https://logotyp.us/files/shopify.svg"
+      /></a>
+
+      <div class="collapse navbar-collapse">
+        <div class="d-flex align-items-center justify-content-between w-100">
+          <section>
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a-dropdown :trigger="['click']">
+                  <a @click.prevent class="nav-link">
+                    Start<DownOutlined class="nav-link"
+                  /></a>
+                </a-dropdown>
+              </li>
+              <li class="nav-item">
+                <a-dropdown :trigger="['click']">
+                  <a @click.prevent class="nav-link">
+                    Sell<DownOutlined class="nav-link"
+                  /></a>
+                </a-dropdown>
+              </li>
+              <li class="nav-item">
+                <a-dropdown :trigger="['click']">
+                  <a @click.prevent class="nav-link">
+                    Market<DownOutlined class="nav-link"
+                  /></a>
+                </a-dropdown>
+              </li>
+              <li class="nav-item">
+                <a-dropdown :trigger="['click']">
+                  <a @click.prevent class="nav-link">
+                    Manage<DownOutlined class="nav-link"
+                  /></a>
+                </a-dropdown>
+              </li>
+            </ul>
+          </section>
+          <section class="d-flex align-items-center">
+            <ul class="navbar-nav me-3">
+              <li class="nav-item">
+                <a @click.prevent class="nav-link">Pricing</a>
+              </li>
+              <li class="nav-item">
+                <a-dropdown :trigger="['click']">
+                  <a @click.prevent class="nav-link">
+                    Learn<DownOutlined class="nav-link"
+                  /></a>
+                </a-dropdown>
+              </li>
+              <li class="nav-item">
+                <a @click.prevent class="nav-link">Login</a>
+              </li>
+            </ul>
+            <a-button class="fw-500 btn rounded-3">Start Free Trial</a-button>
+          </section>
+        </div>
+      </div>
+    </nav>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import { DownOutlined } from "@ant-design/icons-vue";
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    DownOutlined,
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,78 @@
-import { createApp } from "vue";
+import { createApp, h } from "vue";
 import App from "./App.vue";
+import Form from "ant-design-vue/lib/form";
+import Input from "ant-design-vue/lib/input";
+import Button from "ant-design-vue/lib/button";
+import Layout from "ant-design-vue/lib/layout";
+import Menu from "ant-design-vue/lib/menu";
+import Tag from "ant-design-vue/lib/tag";
+import Dropdown from "ant-design-vue/lib/dropdown";
+import Icon from "ant-design-vue/lib/icon";
+import Card from "ant-design-vue/lib/card";
+import Modal from "ant-design-vue/lib/modal";
+import Table from "ant-design-vue/lib/table";
+import Image from "ant-design-vue/lib/image";
+import Select from "ant-design-vue/lib/select";
+import Divider from "ant-design-vue/lib/divider";
+import Breadcrumb from "ant-design-vue/lib/breadcrumb";
+import Radio from "ant-design-vue/lib/radio";
+import Checkbox from "ant-design-vue/lib/checkbox";
+import Tabs from "ant-design-vue/lib/tabs";
+import Spin from "ant-design-vue/lib/spin";
+import InputNumber from "ant-design-vue/lib/input-number";
+import DatePicker from "ant-design-vue/lib/date-picker";
+import Switch from "ant-design-vue/lib/switch";
+import Avatar from "ant-design-vue/lib/avatar";
+import Col from "ant-design-vue/lib/col";
+import Row from "ant-design-vue/lib/row";
+import Collapse from "ant-design-vue/lib/collapse";
+import Popconfirm from "ant-design-vue/lib/popconfirm";
+import Empty from "ant-design-vue/lib/empty";
+import TimePicker from "ant-design-vue/lib/time-picker";
+import { Pagination, Upload, Tooltip } from "ant-design-vue";
+import Drawer from "ant-design-vue/lib/drawer";
+import "@/assets/styles/index.css";
 
-createApp(App).mount("#app");
+const app = createApp({
+  render: () => h(App),
+});
+
+const antdComponents = [
+  Form,
+  Input,
+  Layout,
+  Menu,
+  Tag,
+  Dropdown,
+  Icon,
+  Card,
+  Modal,
+  Table,
+  Image,
+  Button,
+  Select,
+  Divider,
+  Breadcrumb,
+  Radio,
+  Checkbox,
+  Tabs,
+  Spin,
+  InputNumber,
+  Popconfirm,
+  DatePicker,
+  Switch,
+  Col,
+  Row,
+  Avatar,
+  Empty,
+  Pagination,
+  TimePicker,
+  Upload,
+  Tooltip,
+  Collapse,
+  Drawer,
+];
+
+antdComponents.forEach((component) => app.use(component));
+
+app.mount("#app");
